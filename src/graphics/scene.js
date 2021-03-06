9fe1ed28c0c8db05f944f368776290cb59a308f9
Julien Graziano <julien@graziano.fr>
2021-03-06 16:51:37 +0100

    refactor: add Scene

---
+++ src/graphics/scene.js
@@ -0,0 +1,11 @@
+import Color from "./color"
+export default function Scene(painter) {
+    const scene = {
+        backgroundColor: Color.rgb(0, 0, 0).cssrgb,
+    }
+    return {
+        render() {
+            painter.clear(scene.backgroundColor)
+        }
+    }
+}
