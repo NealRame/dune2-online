48227132871b70609ca9dfde432bd4e8b1968d2d
Julien Graziano <julien@graziano.fr>
2021-03-07 01:00:53 +0100

    refactor: Vector is a class now

--- src/maths/vector.js
+++ src/maths/vector.js
@@ -1,75 +1,65 @@
-export default function Vector({x, y}) {
-    return {
-        get x() { return x },
-        get y() { return y },
-        set x(x_) { x = x_ },
-        set y(y_) { y = y_ },
-        get norm() {
-            return Math.sqrt(x*x + y*y)
-        },
-        get opposite() {
-            return Vector({x: -x, y: -y})
-        },
-        mutAdd({x: x_, y: y_}) {
-            x += x_
-            y += y_
-            return this
-        },
-        add({x: x_, y: y_}) {
-            return Vector({x: x + x_, y: y + y_})
-        },
-        mutSub({x: x_, y: y_}) {
-            x -= x_
-            y -= y_
-            return this
-        },
-        sub({x: x_, y: y_}) {
-            return Vector({x: x - x_, y: y - y_})
-        },
-        mutMul(k) {
-            x *= k
-            y *= k
-            return this
-        },
-        mul(k) {
-            return Vector({x: k*x, y: k*y})
-        },
-        mutTransform({m11, m12, m21, m22}) {
-            x = x*m11 + y*m12
-            y = x*m21 + y*m22
-            return this
-        },
-        transform({m11, m12, m21, m22}) {
-            return Vector({
-                x: x*m11 + y*m12,
-                y: x*m21 + y*m22,
-            })
-        },
-        scalar({x: x_, y: y_}) {
-            return x*x_ + y*y_
-        },
-        distance({x: x_, y: y_}) {
-            return Math.sqrt((x - x_)*(x - x_) + (y - y_)*(y - y_))
-        },
-        equal({x: x_, y: y_}) {
-            return x === x_
-                && y === y_
-        },
-        isNull() {
-            return x === 0
-                && y === 0
-        },
-        toUnit() {
-            return this.mul(1/this.norm)
-        },
-        toString() {
-            return `(${x},${y})`
-        }
+export default class Vector {
+    constructor({x, y}) {
+        this.x = x
+        this.y = y
     }
-}
+    get norm() {
+        return Math.sqrt(this.x*this.x + this.y*this.y)
+    }
+    get opposite() {
+        return new Vector({
+            x: -this.x,
+            y: -this.y
+        })
+    }
+    add({x, y}) {
+        this.x += x
+        this.y += y
+        return this
+    }
+    sub({x, y}) {
+        this.x -= x
+        this.y -= y
+        return this
+    }
+    mul(k) {
+        this.x *= k
+        this.y *= k
+        return this
+    }
+    transform({m11, m12, m21, m22}) {
+        const {x, y} = this
+        this.x = x*m11 + y*m12
+        this.y = x*m21 + y*m22
+        return this
+    }
+    scalar({x, y}) {
+        return this.x*x + this.y*y
+    }
+    distance({x, y}) {
+        x = this.x - x
+        y = this.y - y
+        return Math.sqrt(x*x + y*y)
+    }
+    equal({x, y}) {
+        return this.x === x
+            && this.y === y
+    }
+    isNull() {
+        return this.x === 0
+            && this.y === 0
+    }
+    toUnit() {
+        const n = this.norm
+        return new Vector({x: this.x/n, y: this.y/n})
+    }
+    toString() {
+        return `(${this.x},${this.y})`
+    }
 
-Vector.Null  = Vector({x:  0, y:  0})
-Vector.Up    = Vector({x:  0, y: -1})
-Vector.Right = Vector({x:  1, y:  0})
-Vector.Down  = Vector({x:  0, y:  1})
-Vector.Left  = Vector({x: -1, y:  0})
+    static Null  = new Vector({x:  0, y:  0})
+    static Up    = new Vector({x:  0, y: -1})
+    static Right = new Vector({x:  1, y:  0})
+    static Down  = new Vector({x:  0, y:  1})
+    static Left  = new Vector({x: -1, y:  0})
+}
