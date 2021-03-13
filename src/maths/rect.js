e448979db6fb1450415b1b9c1574f75059d4b0e5
Julien Graziano <julien@graziano.fr>
2021-03-13 13:39:36 +0100

    refactor!: update Rect interface

--- src/maths/rect.js
+++ src/maths/rect.js
@@ -12,66 +12,147 @@ import clamp from "lodash/clamp"
  * @class Rect
  */
 export default class Rect {
+    #x
+    #y
     #width
     #height
-    #bottomLeft
-    #bottomRight
-    #center
-    #topLeft
-    #topRight
+
     /**
-     * 
+     * Constructs Rect based on the top left point and a size
      * @param {Point} point
      * @param {Size} size
      */
     constructor({x, y}, {width, height}) {
+        this.#x = x
+        this.#y = y
         this.#width = width
         this.#height = height
-        this.#bottomLeft  = Vector({x, y: y + height})
-        this.#bottomRight = Vector({x: x + width, y: y + height})
-        this.#center      = Vector({x: x + width/2, y: y + height/2})
-        this.#topLeft     = Vector({x, y})
-        this.#topRight    = Vector({x: x + width, y: y})
     }
+
+    /**
+     * @returns {number} the left x coordinate of this Rect
+     */
+    get x() { return this.#x }
+
+    /**
+     * @returns {number} the top y coordinate of this Rect
+     */
+    get y() { return this.#y }
+
+    /**
+     * @returns {number} the width of this Rect
+     */
     get width() { return this.#width }
+
+    /**
+     * @returns {number} the height of this Rect
+     */
     get height() { return this.#height }
-    get leftX() { return this.#topLeft.x }
-    get rightX() { return this.#topRight.x }
-    get topY() { return this.#topLeft.y }
-    get bottomY() { return this.#bottomLeft.y }
-    get topLeft() { return this.#topLeft }
-    get topRight() { return this.#topRight }
-    get bottomLeft() { return this.#bottomLeft }
-    get bottomRight() { return this.#bottomRight }
-    get center() { return this.#center }
+
+    /**
+     * @returns {Size} the size to this Rect
+     */
     get size() {
         return {
             width: this.#width,
             height: this.#height,
         }
     }
+
+    /**
+     * Alias Rect#x
+     * @returns {number}
+     */
+    get leftX() { return this.#x }
+
+    /**
+     * @returns {number} the right x coordinate of this Rect
+     */
+    get rightX() { return this.#x + this.#width }
+
+    /**
+     * Alias Rect#y
+     * @returns {number}
+     */
+    get topY() { return this.#y }
+
+    /**
+     * @returns {number} the bottom y coordinate of this Rect
+     */
+    get bottomY() { return this.#y + this.#height }
+
+    /**
+     * @returns {Vector} the top left point of this Rect
+     */
+    topLeft() {
+        return new Vector({
+            x: this.#x,
+            y: this.#y
+        })
+    }
+
+    /**
+     * @returns {Vector} the top right point of this Rect
+     */
+    topRight() {
+        return new Vector({
+            x: this.rightX,
+            y: this.#y
+        })
+    }
+
+    /**
+     * @returns {Vector} the bottom left point of this Rect
+     */
+    bottomLeft() {
+        return new Vector({
+            x: this.#x,
+            y: this.#y + this.#height
+        })
+    }
+
+    /**
+     * @returns {Vector} the bottom right point of this Rect
+     */
+    bottomRight() {
+        return new Vector({
+            x: this.rightX,
+            y: this.bottomY
+        })
+    }
+
+    /**
+     * @returns {Vector} the center point of this Rect
+     */
+    center() {
+        return new Vector({
+            x: this.#x + this.#width/2,
+            y: this.#y + this.#height/2
+        })
+    }
+
     /**
      * 
-     * @param {Point} point
+     * @param {Point} point a Point like object
      * @returns {boolean}
      */
     contains({x, y}) {
-        return x >= this.#bottomLeft.x  && y <= this.#bottomLeft.y
-            && x >= this.#topLeft.x     && y >= this.#topLeft.y
-            && x <= this.#topRight.x    && y >= this.#topRight.y
-            && x <= this.#bottomRight.x && y <= this.#bottomRight.y
+        return x >= this.leftX && x <= this.rightX
+            && y >= this.bottomY && y <= this.topY
     }
+
     /**
      * 
      * @param {Rect} rect
      * @returns {boolean}
      */
     intersects(rect) {
-        return this.contains(rect.topRight)
-            || this.contains(rect.bottomRight)
-            || this.contains(rect.bottomLeft)
-            || this.contains(rect.topLeft)
+        return this.contains(rect.topRight())
+            || this.contains(rect.bottomRight())
+            || this.contains(rect.bottomLeft())
+            || this.contains(rect.topLeft())
     }
+
     /**
      * 
      * @param {Rect} rect 
@@ -86,6 +167,7 @@ export default class Rect {
             height: clamp(this.height - rect.topY, 0, rect.height),
         })
     }
+
     /**
      * 
      * @param {Size} size
@@ -100,6 +182,7 @@ export default class Rect {
             height: clamp(this.height, 0, height),
         })
     }
+
     /**
      * 
      * @param {Point} vector
@@ -107,10 +190,11 @@ export default class Rect {
      */
     translated({x, y}) {
         return Rect(
-            Vector({x: this.topLeft, y: this.topY}).add({x, y}), 
+            {x: this.x + x, y: this.y + y},
             this.size
         )
     }
+
     /**
      * 
      * @param {(number | {x: number, y: number})} f scale factor
