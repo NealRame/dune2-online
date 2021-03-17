8e222de299c375f98a68385b16e2029515e39aef
Julien Graziano <julien@graziano.fr>
2021-03-17 23:43:25 +0100

    refactor: emit mouse motion event

--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -12,11 +12,13 @@ canvas {
 
 <script>
 import debounce from "lodash/debounce"
+import clamp from "lodash/clamp"
 
 import Painter from "../graphics/painter"
 import Scene from "../graphics/scene"
 
 export default {
+    emits: ["mouseMotion"],
     data() {
         return {
             width: 0,
@@ -36,12 +38,19 @@ export default {
             const {x: left_pos} = this.canvas.getBoundingClientRect()
             this.width = window.innerWidth - left_pos
             this.height = window.innerHeight
+        },
+        mouseMove(e) {
+            const {left, top} = this.canvas.getBoundingClientRect()
+            const x = clamp(e.clientX - left, 0, this.width)
+            const y = clamp(e.clientY - top, 0, this.height)
+            this.$emit("mouseMotion", {x, y})
         }
     },
     created() {
         window.addEventListener("resize", debounce(this.resize, 40))
     },
     mounted() {
+        this.canvas.addEventListener("mousemove", this.mouseMove)
         this.resize()
 
         const scene = Scene(new Painter(this.context))
