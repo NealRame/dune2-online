b540ac17aab9d16a6b15dae71eb03a066de5116b
Julien Graziano <julien@graziano.fr>
2021-03-17 23:39:45 +0100

    refactor: draw grid

--- src/graphics/scene.js
+++ src/graphics/scene.js
@@ -1,20 +1,52 @@
 import {rgb} from "./color"
 
+/**
+ * @typedef {import("../graphics/painter").default} Painter
+ * @typedef {import("../maths/rect").default} Rect
+ */
+
+function draw_grid(painter, {
+    space = 16,
+    color = "#222",
+}) {
+    const {width, height} = painter.size
+    painter.pen = color
+    // Draw vertical grid lines
+    for (let x = .5; x < width; x += space) {
+        painter.drawLine(
+            {x, y: 0},
+            {x, y: height}
+        )
+    }
+    // Draw horizontal grid lines
+    for (let y = .5; y < height; y += space) {
+        painter.drawLine(
+            {y, x: 0},
+            {y, x: width}
+        )
+    }
+}
+
+/**
+ * 
+ * @param {Painter} painter
+ * @returns {*}
+ */
 export default function Scene(painter) {
     const scene = {
         backgroundColor: rgb(0, 0, 0).cssrgb,
-        gridEnabled: false,
-        gridSpace: 16,
+        grid: {
+            enabled: true,
+            space: 32
+        }
     }
     return {
         render() {
-            const {width, height} = painter.size
-            const x = width/2 - 100 + .5
-            const y = height/2 - 100 + .5
-
             painter.clear(scene.backgroundColor)
-            painter.pen = "white"
-            painter.drawRect({x, y, width: 200, height: 200})
+            // draw grid
+            if (scene.grid.enabled) {
+                draw_grid(painter, scene.grid)
+            }
         },
         run() {
             const loop = () => {
