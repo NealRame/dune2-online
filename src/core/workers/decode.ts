9fdc6c4acccec88576595a8aab67a72ee7cf2fad
Julien Graziano <julien@graziano.fr>
2021-04-06 22:33:01 +0200

    fix: base64 to Uint8Array decoding

--- src/core/workers/decode.ts
+++ src/core/workers/decode.ts
@@ -4,6 +4,10 @@ import { ungzip } from "pako"
 
 import registerPromiseWorker from "promise-worker/register"
 
+function base64ToByteArray(b64String: string) {
+    return new Uint8Array(atob(b64String).split("").map(c => c.charCodeAt(0)))
+}
+
 function decodePalette(paletteData: string): unknown {
     return (JSON.parse(paletteData) as Array<RGBColor>).map((color, index) => {
         const alpha = index > 0 ? 255 : 0
@@ -21,7 +25,7 @@ function decodeSoundsets(soundsetsData: string): unknown {
                 && key !== "Ordos"
                 && key !== "FX"
             ) {
-                return new TextEncoder().encode(atob(value as string))
+                return new Uint8Array(atob(value as string).split("").map(c => c.charCodeAt(0)))
             }
             return value
         }
@@ -33,7 +37,7 @@ function decodeTilesets(tilesetsData: string): unknown {
         tilesetsData,
         function (key: string, value: unknown) {
             if (key === "data" || key === "remap") {
-                return new TextEncoder().encode(atob(value as string))
+                return base64ToByteArray(value as string)
             } else if (key === "w") {
                 this.width  = value
             } else if (key === "h") {
@@ -52,7 +56,7 @@ registerPromiseWorker(async ({
     const inflatedData = (new TextDecoder()).decode(ungzip(data))
 
     switch (item) {
-    case "palette":  return decodePalette(inflatedData)
+    case "palette":   return decodePalette(inflatedData)
     case "soundsets": return decodeSoundsets(inflatedData)
     case "tilesets":  return decodeTilesets(inflatedData)
     default:
