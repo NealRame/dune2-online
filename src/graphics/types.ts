38821b8c6421e4ec6ddd0a62b35ac593b9ec3005
Julien Graziano <julien@graziano.fr>
2021-04-13 22:53:50 +0200

    refactor: declare SceneItem interface

--- src/graphics/types.ts
+++ src/graphics/types.ts
@@ -1,5 +1,10 @@
 import { Painter } from "./painter"
+import { RectangularCoordinates, Size } from "@/maths"
 
 export interface PaintDevice {
     painter(): Painter
+}
+
+export interface SceneItem extends RectangularCoordinates, Size {
+    draw(painter: Painter): SceneItem
 }
