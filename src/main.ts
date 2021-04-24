31b7caf8024972b950f697261a8f2f5a5540c724
Julien Graziano <julien@graziano.fr>
2021-04-24 13:59:13 +0200

    refactor: configure fontawesome vue components

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,13 @@ import router from "@/router"
 
 import { createApp } from "vue"
 
-createApp(App).use(router).mount("#app")
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { faRandom } from "@fortawesome/free-solid-svg-icons"
+
+library.add(faRandom)
+
+createApp(App)
+    .use(router)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount("#app")
