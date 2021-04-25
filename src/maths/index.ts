909e1d2a26112e242289c254baf8877ce39533b6
Julien Graziano <julien@graziano.fr>
2021-04-25 23:14:46 +0200

    refactor: add rangeMapper higher-order function

--- src/maths/index.ts
+++ src/maths/index.ts
@@ -1,4 +1,5 @@
 export * from "./types"
+export * from "./noise"
+export * from "./range"
 export { Rect } from "./rect"
 export { Vector } from "./vector"
-export { Noise2DGenerator } from "./noise"
---
+++ src/maths/range.ts
@@ -0,0 +1,16 @@
+/**
+ * Create a range mapping function.
+ * @param iMin input range lower bound
+ * @param iMax input range upper bound
+ * @param oMin output range lower bound
+ * @param oMax output range upper bound
+ * @returns a function
+ */
+export function rangeMapper(
+    iMin: number,
+    iMax: number,
+    oMin: number,
+    oMax: number,
+): (n: number) => number {
+    return (n: number) => (n - iMin)/(iMax - iMin)*(oMax - oMin) + oMin
+}
