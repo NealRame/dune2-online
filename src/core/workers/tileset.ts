081305e4e6c1de4bd66a2b59de2b0dfa73caf037
Julien Graziano <julien@graziano.fr>
2021-04-29 00:06:04 +0200

    refactor: remove file

--- src/core/workers/tileset.ts
+++
@@ -1,9 +0,0 @@
-import { CreateTileMessage, createTile } from "@/core"
-
-import registerPromiseWorker from "promise-worker/register"
-
-registerPromiseWorker(async (message: CreateTileMessage) => {
-    return message.tilesData.map(tileData => {
-        return createTile(tileData, message.palette)
-    })
-})
