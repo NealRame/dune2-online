9515346729a87ede57d6e280a01b2fcfa4f89a50
Julien Graziano <julien@graziano.fr>
2021-05-16 22:06:54 +0200

    refactor: update tile selection algorithm

--- src/core/map.ts
+++ src/core/map.ts
@@ -1,12 +1,14 @@
 import { tileset } from "./data"
-import { Tile, ScaleFactor, Scene, SceneItem } from "./types"
+import { Tile, Tileset, ScaleFactor, Scene, SceneItem } from "./types"
 
 import { Painter } from "@/graphics"
 import { createNoise2DGenerator, createRangeMapper, Rect, RectangularCoordinates, Size } from "@/maths"
 
-import { clamp, flow, times } from "lodash"
+import { clamp, cond, conforms, constant, flow, isNil, negate, stubTrue, times } from "lodash"
+import { equals } from "lodash/fp"
 
 export type LandMapConfig = {
+    // Noise seed
     seed: number,
     // Terrain values
     terrainScale: number,               // clamped to [16, 64]
@@ -30,36 +32,116 @@ enum TerrainType {
 type Terrain = {
     position: RectangularCoordinates,
     spice: number,
-    type: TerrainType,
-    tile: Tile,
+    type: TerrainType
 }
 
 type MapState = {
-    map: Terrain[],
-    rect: Rect,
+    map: Array<Terrain & { tile: Tile }>,
     parent: Scene | SceneItem | null,
 }
 
-function tileFromTypeAndSpice(type: TerrainType, spice: number): Tile {
-    const terrainTiles = tileset("Terrain")
+type Neighborhood<T extends Terrain> = [T|null, T|null, T|null, T|null]
 
-    switch (type) {
-    case TerrainType.Sand:
-        return terrainTiles[spice > 0 ? 191 : 127]
-    case TerrainType.Dunes:
-        return terrainTiles[spice > 0 ? 207 : 159]
-    case TerrainType.Rock:
-        return terrainTiles[143]
-    case TerrainType.Mountain:
-        return terrainTiles[160]
+function indexToPositionConverter({ width }: Size)
+    : (n: number) => RectangularCoordinates {
+    return n => ({ x: n%width, y: Math.floor(n/width) })
+}
+
+function positionToIndexConverter({ width, height }: Size)
+    : (p: RectangularCoordinates) => number {
+    return ({ x, y }) => {
+        return x >= 0 && x < width && y >= 0 && y < height
+            ? y*width + x
+            : -1
     }
+}
 
-    const _exhaustiveCheck: never = type
-    return _exhaustiveCheck
+function neighborhood<T extends Terrain>(size: Size)
+    : (t: T, m: T[]) => Neighborhood<T> {
+    const positionToIndex = positionToIndexConverter(size)
+    return (terrain, map) => {
+        const { x, y } = terrain.position
+        const north = positionToIndex({ x, y: y - 1 })
+        const east  = positionToIndex({ x: x + 1, y })
+        const south = positionToIndex({ x, y: y + 1 })
+        const west  = positionToIndex({ x: x - 1, y })
+        return [
+            north >= 0 ? map[north] : null,
+            east  >= 0 ? map[east]  : null,
+            south >= 0 ? map[south] : null,
+            west  >= 0 ? map[west]  : null,
+        ]
+    }
+}
+
+function selectTile(
+    terrain: Terrain,
+    neighbors: Neighborhood<Terrain>,
+    tiles: Tileset
+): Tile {
+    const typeMask = neighbors
+        .map((neighbor): number => {
+            const type = (neighbor ?? terrain).type
+            if (terrain.type === TerrainType.Rock) {
+                return (type === TerrainType.Rock || type === TerrainType.Mountain) ? 1 : 0
+            }
+            return type === terrain.type ? 1 : 0
+        })
+        .reduce((prev, cur, index) => prev + (cur << index), 0)
+    const spiceMask = neighbors
+        .map((neighbor): number => {
+            if (isNil(neighbor)) return terrain.spice > 0 ? 1 : 0
+            if (neighbor.type === TerrainType.Sand || neighbor.type === TerrainType.Dunes) return neighbor.spice > 0 ? 1 : 0
+            return 0
+        })
+        .reduce((prev, cur, index) => prev + (cur << index), 0)
+
+    return cond([[
+        conforms({
+            type: equals(TerrainType.Sand),
+            spice: negate(equals(0)),
+        }),
+        () => tiles[176 + spiceMask]
+    ], [
+        conforms({
+            type: equals(TerrainType.Dunes),
+            spice: negate(equals(0)),
+        }),
+        () => tiles[192 + typeMask]
+    ], [
+        conforms({
+            type: equals(TerrainType.Dunes),
+        }),
+        () => tiles[144 + typeMask]
+    ], [
+        conforms({
+            type: equals(TerrainType.Rock),
+        }),
+        () => tiles[128 + typeMask]
+    ], [
+        conforms({
+            type: equals(TerrainType.Mountain),
+        }), () => tiles[160 + typeMask]
+    ], [
+        stubTrue,
+        constant(tiles[127])
+    ]])(terrain)
+}
+
+function terrainTileSelector(size: Size)
+    : (t: Terrain, i: number, m: Terrain[]) => [Terrain, Tile] {
+    const tiles = tileset("Terrain")
+    const neighbors = neighborhood(size)
+
+    return (terrain, index, map) => [
+        terrain,
+        selectTile(terrain, neighbors(terrain, map), tiles)
+    ]
 }
 
 function checkConfig(config: Partial<LandMapConfig>): LandMapConfig {
     return {
+        // Noise seed
         seed: config.seed ?? Date.now(),
         // Terrain values
         terrainScale: clamp(Math.floor(config.terrainScale ?? 32), 16, 64),
@@ -74,17 +156,8 @@ function checkConfig(config: Partial<LandMapConfig>): LandMapConfig {
     }
 }
 
-function createTerrain(width: number, height: number, config: LandMapConfig) {
-    const spiceAmount = flow(
-        createNoise2DGenerator({
-            seed: config.seed + 1,
-            scale: config.spiceScale,
-            octaves: config.spiceDetails,
-        }),
-        createRangeMapper(-1, 1, 0, 1),
-        (n: number) => n < config.spiceThreshold ? 1 : 0
-    )
-    const terrainType = flow(
+function terrainTypeGenerator(config: LandMapConfig): (p: RectangularCoordinates) => TerrainType {
+    return flow(
         createNoise2DGenerator({
             seed: config.seed,
             scale: config.terrainScale,
@@ -104,31 +177,47 @@ function createTerrain(width: number, height: number, config: LandMapConfig) {
             return TerrainType.Mountain
         }
     )
-
-    return times(width*height, (n: number): Terrain => {
-        const position = { x: n%width, y: Math.floor(n/width) }
-        const spice = spiceAmount(position)
-        const type = terrainType(position)
-        const tile = tileFromTypeAndSpice(type, spice)
-        return {
-            position,
-            spice,
-            type,
-            tile,
-        }
-    })
 }
 
-export function createMap({ width, height }: Size, config: Partial<LandMapConfig>): SceneItem {
+function spiceAmountGenerator(config: LandMapConfig): (p: RectangularCoordinates) => number {
+    return flow(
+        createNoise2DGenerator({
+            seed: config.seed + 1,
+            scale: config.spiceScale,
+            octaves: config.spiceDetails,
+        }),
+        createRangeMapper(-1, 1, 0, 1),
+        (n: number) => n < config.spiceThreshold ? 1 : 0
+    )
+}
+
+function terrainGenerator(config: LandMapConfig): (p: RectangularCoordinates) => Terrain {
+    const generateTerrainType = terrainTypeGenerator(config)
+    const generateSpiceAmount = spiceAmountGenerator(config)
+
+    return position => {
+        const spice = generateSpiceAmount(position)
+        const type = generateTerrainType(position)
+        return { position, spice, type }
+    }
+}
+
+function createTerrain(size: Size, config: LandMapConfig) {
+    return times(size.width*size.height, indexToPositionConverter(size))
+        .map(terrainGenerator(config))
+        .map(terrainTileSelector(size))
+        .map(([terrain, tile]) => Object.assign({}, terrain, { tile }))
+}
+
+export function createMap(size: Size, config: Partial<LandMapConfig>): SceneItem {
     const state: MapState = {
-        map: createTerrain(width, height, checkConfig(config)),
-        rect: new Rect({ x: 0, y: 0 }, { width, height }),
+        map: createTerrain(size, checkConfig(config)),
         parent: null,
     }
 
     const getScale = () => state.parent?.scale ?? 1
-    const getWidth = () => width*(state.map[0]?.tile[getScale()].width ?? 0)
-    const getHeight = () => height*(state.map[0]?.tile[getScale()].height ?? 0)
+    const getWidth = () => size.width*(state.map[0]?.tile[getScale()].width ?? 0)
+    const getHeight = () => size.height*(state.map[0]?.tile[getScale()].height ?? 0)
 
     return {
         get x(): number { return 0 },
