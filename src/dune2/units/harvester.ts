9a569a4be67200ac7aaae4754a49b408651ce06a
Julien Graziano <julien@graziano.fr>
2021-06-26 00:05:38 +0200

    refactor: harvester unit

---
+++ src/dune2/units/harvester.ts
@@ -0,0 +1,24 @@
+import { Unit } from "./unit"
+
+import { imageSet } from "@/dune2/data"
+import { Scene } from "@/engine"
+
+import { RectangularCoordinates } from "@/maths"
+
+export class Harvester extends Unit {
+    constructor(scene: Scene, position: RectangularCoordinates) {
+        super(scene, position)
+
+        const images = imageSet("units")
+        const shape = { columns: 1, rows: 1 }
+
+        this.addFrame(shape, [images[16]])
+            .addFrame(shape, [images[17]])
+            .addFrame(shape, [images[19]])
+            .addFrame(shape, [images[21]])
+            .addFrame(shape, [images[23]])
+            .addFrame(shape, [images[22]])
+            .addFrame(shape, [images[20]])
+            .addFrame(shape, [images[18]])
+    }
+}
