ce6b07f5994be621b55910b1032fc8751af29965
Julien Graziano <julien@graziano.fr>
2021-06-26 00:06:28 +0200

    refactor: add quad unit

--- src/dune2/units/index.ts
+++ src/dune2/units/index.ts
@@ -1,1 +1,2 @@
 export * from "./harvester"
+export * from "./quad"
---
+++ src/dune2/units/quad.ts
@@ -0,0 +1,24 @@
+import { Unit } from "./unit"
+
+import { imageSet } from "@/dune2/data"
+import { Scene } from "@/engine"
+
+import { RectangularCoordinates } from "@/maths"
+
+export class Quad extends Unit {
+    constructor(scene: Scene, position: RectangularCoordinates) {
+        super(scene, position)
+
+        const images = imageSet("units")
+        const shape = { columns: 1, rows: 1 }
+
+        this.addFrame(shape, [images[0]])
+            .addFrame(shape, [images[1]])
+            .addFrame(shape, [images[3]])
+            .addFrame(shape, [images[5]])
+            .addFrame(shape, [images[7]])
+            .addFrame(shape, [images[6]])
+            .addFrame(shape, [images[4]])
+            .addFrame(shape, [images[2]])
+    }
+}
