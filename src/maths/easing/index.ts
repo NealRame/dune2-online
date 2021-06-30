870b4507c9d19fdaf69db3e4b62c06f5fb266cbe
Julien Graziano <julien@graziano.fr>
2021-06-30 23:44:10 +0200

    refactor: add sequence generator

--- src/maths/easing/index.ts
+++ src/maths/easing/index.ts
@@ -1,3 +1,7 @@
+import { linspace } from "../linspace"
+
+export type EasingFunction = (t: number) => number
+
 export function linear(t: number): number {
     return t
 }
@@ -5,3 +9,10 @@ export function linear(t: number): number {
 export * as Quad from "./quad"
 export * as Cubic from "./cubic"
 export * as Exp from "./exp"
+
+export function * sequence(count: number, easingFunction: EasingFunction)
+    : Generator<ReturnType<EasingFunction>> {
+    for (const t of linspace(count)) {
+        yield easingFunction(t)
+    }
+}
