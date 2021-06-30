289896381375146901093c847ae9b7eb1660fc01
Julien Graziano <julien@graziano.fr>
2021-06-30 23:43:17 +0200

    refactor: linspace is a generator now

--- src/maths/linspace.ts
+++ src/maths/linspace.ts
@@ -1,14 +1,15 @@
-import { range } from "lodash"
-
 export type LinSpaceConfig = {
     first?: number,
     last?: number,
 }
 
-export function linspace(len: number, config: LinSpaceConfig = {}): number[] {
+export function * linspace(count: number, config: LinSpaceConfig = {})
+    : Generator<number> {
     const first = config.first ?? 0
-    const last = config.last ?? 0
-    const a = range(first, last, (last - first)/(len - 1))
-    a.push(last)
-    return a
+    const last = config.last ?? 1
+    const step = (last - first)/(count - 1)
+
+    for (let i = 0; i < count; ++i) {
+        yield i*step
+    }
 }
