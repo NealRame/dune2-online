16c80aeaa42cc9b4d01f4b1c89968e8374dd4f4d
Julien Graziano <julien@graziano.fr>
2021-07-01 23:02:07 +0200

    refactor: update Unit class

--- src/dune2/units/unit.ts
+++ src/dune2/units/unit.ts
@@ -1,39 +1,72 @@
-import { RectangularCoordinates, Vector } from "@/maths"
-import { Scene, Sprite } from "@/engine"
-import { clamp, isNil } from "lodash"
+import { sequence, Easing, RectangularCoordinates, Vector } from "@/maths"
+import { Direction, Scene, Sprite } from "@/engine"
+import { isNil } from "lodash"
+
+function direction(d: Direction): Vector {
+    switch (d) {
+    case Direction.North:
+        return Vector.Up()
+
+    case Direction.Northeast:
+        return Vector.UpRight()
+
+    case Direction.East:
+        return Vector.Right()
+
+    case Direction.Southeast:
+        return Vector.DownRight()
+
+    case Direction.South:
+        return Vector.Down()
+
+    case Direction.Southwest:
+        return Vector.DownLeft()
+
+    case Direction.West:
+        return Vector.Left()
+
+    case Direction.Northwest:
+        return Vector.UpLeft()
+    }
+
+    const exhaustiveCheck_: never = d
+    return exhaustiveCheck_
+}
+
+function move(position: Vector, direction: Vector, frameCount: number) {
+    return sequence(frameCount, t => {
+        const v = Easing.Quadratic.easeInOut(t)
+        return position.copy().add({
+            x: v*direction.x,
+            y: v*direction.y,
+        })
+    })
+}
 
 export class Unit extends Sprite {
     protected hitPoints_: number
-    protected speedMax_: number
-    protected direction_: Vector | null
-    protected destination_: Vector | null
+    private destination_: Iterator<Vector> | null
 
     constructor(scene: Scene, position: RectangularCoordinates) {
         super(scene, position)
         this.hitPoints_ = 1
-        this.speedMax_ = 0.05
-        this.direction_ = null
         this.destination_ = null
     }
 
-    move({ x, y }: RectangularCoordinates): Unit {
-        this.direction_ = (new Vector(x, y)).toUnit()
-        this.destination_ = this.position.add(this.direction_)
+    move(d: Direction): Unit {
+        if (isNil(this.destination_)) {
+            this.destination_ = move(this.position, direction(d), 60)
+        }
         return this
     }
 
     update(): Unit {
         if (!isNil(this.destination_)) {
-            const d = this.position.distance(this.destination_)
-            if (d > 0.001) {
-                const s = clamp(4*(d - d*d)*this.speedMax_, d === 1 ? 0.005 : 0, 1)
-                this.position = (this.direction_?.copy() ?? Vector.Null()).mul(s).add({
-                    x: this.x,
-                    y: this.y,
-                })
-            } else {
-                this.position = this.destination_
+            const it = this.destination_.next()
+            if (it.done) {
                 this.destination_ = null
+            } else {
+                this.position = it.value
             }
         }
         return this
