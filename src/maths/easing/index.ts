07ad58613596549b89f4e9c9072462d8b39cf93c
Julien Graziano <julien@graziano.fr>
2021-07-10 23:23:47 +0200

    refactor: add step easing function

--- src/maths/easing/index.ts
+++ src/maths/easing/index.ts
@@ -4,6 +4,10 @@ export function linear(t: number): number {
     return t
 }
 
+export function step(count: number): (t: number) => number {
+    return t => Math.floor(count*t)/count
+}
+
 export * as Quadratic from "./quadratic"
 export * as Cubic from "./cubic"
 export * as Exp from "./exp"
