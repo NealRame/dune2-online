3dfb0563bb9c37f95951a31a29437ebe45f80e90
Julien Graziano <julien@graziano.fr>
2021-07-11 20:55:37 +0200

    refactor: use Animation module

--- src/dune2/units/unit.ts
+++ src/dune2/units/unit.ts
@@ -1,8 +1,16 @@
-import { Direction, DirectionCount, Scene, Sprite } from "@/engine"
-import { linspace, Easing, RectangularCoordinates, Vector } from "@/maths"
+import {
+    Animation,
+    createSequenceAnimation,
+    createTransitionAnimation,
+    Direction,
+    DirectionCount,
+    Scene,
+    Sprite,
+} from "@/engine"
+import { Easing, RectangularCoordinates, Vector } from "@/maths"
 import { createObserver, EventCallback, Observer } from "@/utils"
 
-import { isNil, times } from "lodash"
+import { clamp, isNil, times } from "lodash"
 
 function direction(d: Direction): Vector {
     switch (d) {
@@ -35,57 +43,62 @@ function direction(d: Direction): Vector {
     return exhaustiveCheck_
 }
 
-function * createDirectionAnimation(from: Direction, to: Direction, frameCount: number) {
+function directionRotationSequence(from: Direction, to: Direction) {
     const d = (from <= to ? to : to + DirectionCount) - from
     const step = d > 4 ? -1 : 1
-
-    const directions = times(
-        Math.abs(d > 4 ? d - DirectionCount : d),
-        () => {
-            from = (from + step + DirectionCount)%DirectionCount
-            return from
-        }
-    )
-
-    for (const direction of directions) {
-        for (let frame = 0; frame < frameCount; ++frame) {
-            yield direction
-        }
-    }
-}
-
-function * createMoveAnimation(position: Vector, direction: Vector, frameCount: number) {
-    for (const t of linspace(frameCount)) {
-        const v = Easing.Quadratic.easeInOut(t)
-        yield position.copy().add({
-            x: v*direction.x,
-            y: v*direction.y,
-        })
-    }
+    return times(Math.abs(d > 4 ? d - DirectionCount : d), () => {
+        from = (from + step + DirectionCount)%DirectionCount
+        return from
+    })
 }
 
 export class Unit extends Sprite {
     protected hitPoints_: number
-    private destination_: Iterator<Vector> | null
-    private direction_: Iterator<Direction> | null
 
     private moveObserver_: Observer<void>
+    private animation_: Animation|null
 
     constructor(scene: Scene, position: RectangularCoordinates) {
         super(scene, position)
         this.hitPoints_ = 1
-        this.destination_ = null
-        this.direction_ = null
+        this.animation_ = null
         this.moveObserver_ = createObserver()
     }
 
-    move(d: Direction): Unit {
-        if (isNil(this.direction_)) {
-            this.direction_ = createDirectionAnimation(this.frameIndex as Direction, d, 30)
-        }
-        if (isNil(this.destination_)) {
-            this.destination_ = createMoveAnimation(this.position, direction(d), 60)
-        }
+    get direction(): Direction {
+        return this.frameIndex
+    }
+
+    set direction(direction: Direction) {
+        this.frameIndex = direction
+    }
+
+    move(moveDirection: Direction): Unit {
+        const directions = directionRotationSequence(this.direction, moveDirection)
+        const p = this.position
+        const d = direction(moveDirection)
+        this.animation_ = createSequenceAnimation({
+            animations: [
+                createTransitionAnimation({
+                    frames: 30*directions.length,
+                    easing: Easing.step(directions.length),
+                    set: (t: number) => {
+                        this.direction = directions[clamp(Math.floor(t*directions.length), 0, directions.length - 1)]
+                    }
+                }),
+                createTransitionAnimation({
+                    frames: 60,
+                    easing: Easing.Cubic.easeInOut,
+                    set: (t: number) => {
+                        this.position = p.copy().add({
+                            x: t*d.x,
+                            y: t*d.y,
+                        })
+                    }
+                })
+            ],
+            done: () => this.moveObserver_.publish()
+        })
         return this
     }
 
@@ -94,20 +107,11 @@ export class Unit extends Sprite {
     }
 
     update(): Unit {
-        if (!isNil(this.direction_)) {
-            const it = this.direction_.next()
-            if (it.done) {
-                this.direction_ = null
+        if (!isNil(this.animation_)) {
+            if (this.animation_.finished()) {
+                this.animation_ = null
             } else {
-                this.frameIndex = it.value
-            }
-        } else if (!isNil(this.destination_)) {
-            const it = this.destination_.next()
-            if (it.done) {
-                this.destination_ = null
-                this.moveObserver_.publish()
-            } else {
-                this.position = it.value
+                this.animation_.next()
             }
         }
         return this
