2f59ea699e9af4c72561528680c51aedaf24799f
Julien Graziano <julien@graziano.fr>
2021-07-18 23:38:21 +0200

    refactor: game module

---
+++ src/dune2/game.ts
@@ -0,0 +1,34 @@
+import { Unit } from "./units/unit"
+import { Harvester } from "./units/harvester"
+import { Quad } from "./units/quad"
+import { Trike } from "./units/trike"
+
+import { createScene } from "@/engine"
+import { PaintDevice } from "@/graphics"
+import { RectangularCoordinates } from "@/maths"
+
+export const Units = {
+    Harvester,
+    Quad,
+    Trike,
+} as const
+
+export interface Game {
+    addUnit(type: keyof typeof Units, position: RectangularCoordinates): Unit
+    start(): void
+}
+
+export { Unit } from "./units/unit"
+
+export function createGame(screen: PaintDevice): Game {
+    const scene = createScene()
+    return {
+        start(): void {
+
+            scene.run(screen.painter)
+        },
+        addUnit(type, position) {
+            return new Units[type](scene, position)
+        }
+    }
+}
--- src/dune2/units/index.ts
+++ src/dune2/units/index.ts
@@ -1,3 +1,5 @@
+export * from "./unit"
+
 export * from "./harvester"
 export * from "./quad"
 export * from "./trike"
