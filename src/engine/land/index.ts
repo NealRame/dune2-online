7dab852213b32251698c4e6d185c5bab38ef03b5
Julien Graziano <julien@graziano.fr>
2021-08-10 17:56:50 +0200

    refactor: move Zone definition

--- src/engine/land/index.ts
+++ src/engine/land/index.ts
@@ -1,13 +1,13 @@
 import { createPositionToZoneConverter } from "./utils"
+import { Zone } from "./zone"
 
-import { AbstractSceneItem } from "@/engine/scene"
-import { Image, ScaleFactors, Scene, SceneItem } from "@/engine/types"
+import { Image, Scene, SceneItem } from "@/engine/types"
 
 import { Color, Painter } from "@/graphics"
 import { Rect, RectangularCoordinates, Size, Vector } from "@/maths"
+import { createObserver, Observer } from "@/utils"
 
 import { chain, isNil } from "lodash"
-import { createObserver, Observer } from "@/utils"
 
 function * zoneIterator(
     rect: Rect|null,
@@ -24,74 +24,6 @@ function * zoneIterator(
                 size: { width, height },
             }
         }
-    }
-}
-
-class Zone extends AbstractSceneItem {
-    private image_: Partial<Image> = {}
-    private redraw_: [RectangularCoordinates, Image[]][] = []
-
-    constructor(scene: Scene, rect: Rect) {
-        super(scene)
-        this.x = rect.x
-        this.y = rect.y
-        this.width_ = rect.width
-        this.height_ = rect.height
-    }
-
-    refresh(terrain: Terrain): Zone {
-        this.redraw_.push([
-            terrain.position,
-            terrain.image(),
-        ])
-        return this
-    }
-
-    update(): Zone {
-        const { gridUnit } = this.scene
-        const { width, height } = this.rect
-
-        for (const scale of ScaleFactors) {
-            const gridSpacing = gridUnit*scale
-            const canvas = new OffscreenCanvas(gridSpacing*width, gridSpacing*height)
-            const context = canvas.getContext("2d") as OffscreenCanvasRenderingContext2D
-
-            if (!isNil(this.image_[scale])) {
-                context.drawImage(this.image_[scale] as ImageBitmap, 0, 0)
-            }
-
-            for (const [position, images] of this.redraw_) {
-                for (const image of images) {
-                    context.drawImage(
-                        image[scale],
-                        gridSpacing*(position.x - this.x),
-                        gridSpacing*(position.y - this.y),
-                    )
-                }
-            }
-
-            this.image_[scale] = canvas.transferToImageBitmap()
-        }
-        this.redraw_ = []
-        return this
-    }
-
-    render(
-        painter: Painter,
-        viewport: Rect,
-    ): Zone {
-        const { gridSpacing, scale } = this.scene
-
-        if (this.rect.intersects(viewport)) {
-            if (!isNil(this.image_[scale])) {
-                painter.drawImageBitmap(
-                    this.image_[scale] as ImageBitmap,
-                    this.position.sub(viewport).mul(gridSpacing)
-                )
-            }
-        }
-
-        return this
     }
 }
 
---
+++ src/engine/land/zone.ts
@@ -0,0 +1,75 @@
+import { Terrain } from "."
+
+import { AbstractSceneItem, Image, ScaleFactors, Scene } from "@/engine"
+import { Painter } from "@/graphics"
+import { RectangularCoordinates, Rect } from "@/maths"
+
+import { isNil } from "lodash"
+
+export class Zone extends AbstractSceneItem {
+    private image_: Partial<Image> = {}
+    private redraw_: [RectangularCoordinates, Image[]][] = []
+
+    constructor(scene: Scene, rect: Rect) {
+        super(scene)
+        this.x = rect.x
+        this.y = rect.y
+        this.width_ = rect.width
+        this.height_ = rect.height
+    }
+
+    refresh(terrain: Terrain): Zone {
+        this.redraw_.push([
+            terrain.position,
+            terrain.image(),
+        ])
+        return this
+    }
+
+    update(): Zone {
+        const { gridUnit } = this.scene
+        const { width, height } = this.rect
+
+        for (const scale of ScaleFactors) {
+            const gridSpacing = gridUnit*scale
+            const canvas = new OffscreenCanvas(gridSpacing*width, gridSpacing*height)
+            const context = canvas.getContext("2d") as OffscreenCanvasRenderingContext2D
+
+            if (!isNil(this.image_[scale])) {
+                context.drawImage(this.image_[scale] as ImageBitmap, 0, 0)
+            }
+
+            for (const [position, images] of this.redraw_) {
+                for (const image of images) {
+                    context.drawImage(
+                        image[scale],
+                        gridSpacing*(position.x - this.x),
+                        gridSpacing*(position.y - this.y),
+                    )
+                }
+            }
+
+            this.image_[scale] = canvas.transferToImageBitmap()
+        }
+        this.redraw_ = []
+        return this
+    }
+
+    render(
+        painter: Painter,
+        viewport: Rect,
+    ): Zone {
+        const { gridSpacing, scale } = this.scene
+
+        if (this.rect.intersects(viewport)) {
+            if (!isNil(this.image_[scale])) {
+                painter.drawImageBitmap(
+                    this.image_[scale] as ImageBitmap,
+                    this.position.sub(viewport).mul(gridSpacing)
+                )
+            }
+        }
+
+        return this
+    }
+}
