dc51e86a66a04116fb846f33a7d1ed8f1c67169a
Julien Graziano <julien@graziano.fr>
2021-08-13 17:53:18 +0200

    style: remove commas

--- src/engine/types.ts
+++ src/engine/types.ts
@@ -8,19 +8,26 @@ export type ScaleFactor = typeof ScaleFactors[number]
 export type Image = Record<ScaleFactor, ImageBitmap>
 
 export interface Scene {
-    scale: ScaleFactor,
-    viewport: Rect | null,
-    readonly gridUnit: number,
-    readonly gridSpacing: number,
-    gridEnabled: boolean,
-    readonly rect: Rect,
-    addItem(item: SceneItem): Scene,
-    clear(): Scene,
-    render(painter: Painter): Scene,
-    run(painter: Painter): Scene,
-    update(): Scene,
+    scale: ScaleFactor
+    viewport: Rect | null
+    readonly gridUnit: number
+    readonly gridSpacing: number
+    gridEnabled: boolean
+    readonly rect: Rect
+    addItem(item: SceneItem): Scene
+    clear(): Scene
+    render(painter: Painter): Scene
+    run(painter: Painter): Scene
+    update(): Scene
 }
 
 export interface SceneItem {
+    readonly scene: Scene
+    readonly width: number
+    readonly height: number
+    readonly size: Size
+    readonly rect: Rect
+    readonly position: Vector
+    render(painter: Painter, viewport: Rect): SceneItem
     update(): SceneItem
 }
