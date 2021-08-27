6cb33e5f401b869c596bba27e412d90dbe8656b5
Julien Graziano <julien@graziano.fr>
2021-08-27 11:24:31 +0200

    refactor: update createGame

--- src/engine/game.ts
+++ src/engine/game.ts
@@ -5,6 +5,12 @@ import { Unit } from "./unit"
 
 import { PaintDevice } from "@/graphics"
 import { Size } from "@/maths"
+import { isNil } from "lodash"
+
+interface GameState {
+    animationRequestId: number
+    scene: Scene
+}
 
 export interface GameConfig<T extends Terrain = Terrain> {
     screen: PaintDevice
@@ -20,39 +26,42 @@ export interface Game<T extends Terrain = Terrain> {
     stop(): Game<T>
 }
 
-export function createGame<T extends Terrain = Terrain>(config: GameConfig<T>): Game<T> {
-    let requestAnimationId = 0
-    const scene = createScene(config.size, config.screen.painter)
-    const land = createLand(scene, config)
+export function createGame<T extends Terrain = Terrain>(config: GameConfig<T>)
+    : Game<T> {
+    const state: GameState = {
+        animationRequestId: 0,
+        scene: createScene(config.size, config.screen.painter)
+    }
 
-    scene.addLayer(land)
-
-    // const structureLayer = scene.addLayer("structures")
-    const unitLayer = scene.addLayer("units")
+    state.scene.addLayer(createLand(state.scene, config))
+    state.scene.addLayer("units")
 
     return {
         get scene() {
-            return scene
+            return state.scene
         },
         get land() {
-            return land
+            return state.scene.getLayer("land") as Land<T>
         },
         addUnit(unit: Unit) {
-            unitLayer.addItem(unit)
+            const unitLayer = state.scene.getLayer("units")
+            if (!isNil(unitLayer)) {
+                unitLayer.addItem(unit)
+            }
             return this
         },
         start(): Game<T> {
             (function animationLoop() {
-                scene
+                state.scene
                     .update()
                     .render()
-                requestAnimationId = requestAnimationFrame(animationLoop)
+                state.animationRequestId = requestAnimationFrame(animationLoop)
             })()
             return this
         },
         stop(): Game<T> {
-            cancelAnimationFrame(requestAnimationId)
-            requestAnimationId = 0
+            cancelAnimationFrame(state.animationRequestId)
+            state.animationRequestId = 0
             return this
         }
     }
