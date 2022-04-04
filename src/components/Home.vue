eb8c07a3ef0b53f38cd1638cb80af8b2b2b932cd
Julien Graziano <julien@graziano.fr>
2022-04-04 23:30:42 +0200

    refactor: update Homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,57 @@
+<style lang="scss" scoped>
+.homepage-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    margin: 0 auto;
+    padding: 0;
+
+    transform: translate(-50%, -50%);
+
+    & > h1 {
+        border-bottom: 1px solid $fg-color;
+
+        color: $fg-color;
+
+        font-family: "dune-rise";
+        font-size: 4rem;
+        font-weight: 100;
+
+        text-align: center;
+
+        margin: 0 0 2rem;
+        padding: 0 0 .5rem;
+    }
+
+    & > ul {
+        margin: 0 auto;
+        padding: 0;
+
+        list-style: none;
+
+        & > li > a {
+            border: 1px solid transparent;
+            display: block;
+            height: 100%;
+
+            text-transform: uppercase;
+
+            &:hover {
+                background-color: darken($color: $link-color, $amount: 45%);
+                border: 1px solid darken($color: $link-color, $amount: 35%);
+            }
+        }
+    }
+}
+</style>
+
 <template>
-    <h1>Dune</h1>
-    <ul class="game-menu">
-        <li><router-link to="/game">Game</router-link></li>
-        <!-- <li><router-link to="/editor">Editor</router-link></li> -->
-    </ul>
+    <div class="homepage-content">
+        <h1>Dune</h1>
+        <ul class="game-menu">
+            <li><router-link to="/game">Game</router-link></li>
+            <li><router-link to="/editor">Editor</router-link></li>
+        </ul>
+    </div>
 </template>
