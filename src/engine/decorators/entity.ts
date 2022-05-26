62d06da8d73e06a077c24cfb76cb9fb5e6826dd1
Julien Graziano <julien@graziano.fr>
2022-05-26 23:01:50 +0200

    refactor: update Entity decorator

--- src/engine/decorators/entity.ts
+++ src/engine/decorators/entity.ts
@@ -1,14 +1,21 @@
+import { chain } from "lodash"
+
+import {
+    type IEntity,
+    type IEntityData,
+    type IEntityEvents,
+    type MapToIEntityMixins,
+} from "@/engine/entity"
+
 import {
     Service,
     ServiceLifecycle,
+    type TConstructor,
 } from "@/engine/injector"
 
-import { chain } from "lodash"
-
-import { TConstructor } from "../injector/types"
-
-import { IEntity, IEntityData, IEntityEvents } from "../entity"
-import { Image } from "../scene"
+import {
+    type Image
+} from "@/engine/scene"
 
 export const EntityMetadataKey = Symbol("entity:metadata")
 
@@ -22,17 +29,19 @@ export interface IEntityTileProvider<
 export interface IEntityMetadata<
     Data extends IEntityData = IEntityData,
     Events extends IEntityEvents = IEntityEvents,
+    IMixins extends Array<Record<string, unknown>> = [],
 > {
+    EntityMixins: MapToIEntityMixins<Data, Events, IMixins>
     TileProvider: TConstructor<IEntityTileProvider<Data, Events>>
 }
 
-export function Entity(): ClassDecorator {
+export function Entity(metadata: IEntityMetadata): ClassDecorator {
     // eslint-disable-next-line @typescript-eslint/ban-types
     return (target: Function) => {
         chain(target)
             .tap(Service({ lifecycle: ServiceLifecycle.Singleton }))
             .tap(target => {
-                Reflect.defineMetadata(EntityMetadataKey, {}, target)
+                Reflect.defineMetadata(EntityMetadataKey, metadata, target)
             })
     }
 }
